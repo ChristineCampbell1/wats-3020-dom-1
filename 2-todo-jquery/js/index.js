ac95ef8623eac9871705020e70aaf9b6cb32a257
Christine Campbell <ccamp67@gmail.com>
2020-01-29 21:48:25 -0800

    finished 2

--- 2-todo-jquery/js/index.js
+++ 2-todo-jquery/js/index.js
@@ -1,20 +1,23 @@
-//TODO add document ready function
+$(function() {
+  $(document).submit(function(event) { 
+    event.preventDefault();
 {
-  //TODO wrap all code related to user submit  
+ 
   {
-    //TODO add code to keep form default submit behavior from occurring
+    
     // get the value of the input box using jQuery
-    //TODO select the DOM object with the name `item-input` and assign its value to a variable called itemValue
+    let itemInput = $("input[name='item-input']");
+    let itemValue = itemInput.val();
 
     //check that user has input a value before proceeding
     if (itemValue.length !== 0) {
       //create new item
-      // TODO append a new item to the todo list that is a <li> element with two children: an <input> with type checkbox and a <label> with the inner text of the value of itemValue
+      $('.todo-list').append("<li><input type='checkbox'></input><label>" + itemValue + "</label><li>");
  
-      // TODO clear user input in the input box
+      itemInput.val("");
 
       //set up toggle on check box click
-      // TODO add the code to make this function get added to every :checkbox on the change event
+      $(':checkbox').change(function (event) {
       (function (event) {
         if ($(this).is(':checked')) {
           $(this).siblings("label").css("text-decoration", "line-through")
