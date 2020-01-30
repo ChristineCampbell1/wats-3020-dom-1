6eb9bb5da349bd34249ac69c323143bc0e42fab6
Christine Campbell <ccamp67@gmail.com>
2020-01-30 13:19:01 -0800

    finished 2

--- 2-todo-jquery/js/index.js
+++ 2-todo-jquery/js/index.js
@@ -1,28 +1,24 @@
 $(function() {
-  $(document).submit(function(event) { 
+  $(document).submit(function(event){ 
     event.preventDefault();
-{
- 
-  {
     
     // get the value of the input box using jQuery
-    let itemInput = $("input[name='item-input']");
-    let itemValue = itemInput.val();
+    let itemElement = $("[name='item-input']");
+    let itemValue = itemElement.val();
 
     //check that user has input a value before proceeding
     if (itemValue.length !== 0) {
       //create new item
-      $('.todo-list').append("<li><input type='checkbox'></input><label>" + itemValue + "</label><li>");
+      $(".todo-list").append("<li><input type='checkbox'><label>" + itemValue + " </label></li>");
  
-      itemInput.val("");
+      itemElement.val('');
 
       //set up toggle on check box click
-      $(':checkbox').change(function (event) {
-      (function (event) {
+      $(":checkbox").change(function (event) {
         if ($(this).is(':checked')) {
           $(this).siblings("label").css("text-decoration", "line-through")
         } else {
-          $(this).siblings("label").css("text-decoration", "none")
+          $(this).siblings("label").css("text-decoration", "none");
         }
       }) // checkbox event function
 
